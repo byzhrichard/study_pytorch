2b0fe808bdd85e819d8d78b34e325334e06aa5a1
byzh <805278087@qq.com>
2024-03-11 16:08:16 +0800

    修改了transform，能到0.88

---
+++ web_course/ResNet/run_2.py
@@ -0,0 +1,95 @@
+import numpy as np
+import torch
+from torch import nn, optim
+from torchvision import datasets,transforms
+from torch.utils.data import DataLoader
+from ResNet import ResNet18
+from net import ResNet188
+
+def main():
+    batch_size=100
+    cifar_train = datasets.CIFAR10('../cifar', #访问当前目录的cifar文件夹
+                             True,
+                             transform=transforms.Compose([
+        transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+        transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
+    ]),
+                             download=True) #如果没有，那么下载
+    cifar_train = DataLoader(cifar_train, batch_size=batch_size, shuffle=True)
+    # print("awa", len(cifar_train) * batch_size)
+    cifar_test = datasets.CIFAR10('../cifar',
+                                   False,
+                                   transform=transforms.Compose([
+                                       transforms.Resize((32, 32)),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                            std=[0.229, 0.224, 0.225])
+                                   ]),
+                                   download=True)
+    cifar_test = DataLoader(cifar_test, batch_size=batch_size, shuffle=True)
+    # transform_train = transforms.Compose([
+    #     transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+    #     transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+    #     transforms.ToTensor(),
+    #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
+    # ])
+    #
+    # transform_test = transforms.Compose([
+    #     transforms.ToTensor(),
+    #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    # ])
+    # print("awa", len(cifar_test) * batch_size)
+
+    # x, label =iter(cifar_train).__next__()
+    # print("x.shape:",x.shape,"label.shape:",label.shape)
+
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    # model = ResNet18().to(device)
+    model = ResNet188().to(device)
+    criteon = nn.CrossEntropyLoss().to(device)
+    # optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    # optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9,
+                          weight_decay=5e-4)  # 优化方式为mini-batch momentum-SGD，并采用L2正则化（权重衰减）
+    # print(device)
+    for epoch in range(20):
+        f = open("train_out.txt", "a+")  # 追加，可读可写
+        model.train()#训练模式
+        loss_sum = []
+        for batchidx, (x, label) in enumerate(cifar_train):
+            if batchidx%50==0:print(batchidx)
+            # [b,3,32,32]
+            # [b]
+            x, label = x.to(device), label.to(device)
+            logits = model(x)
+            # logits    [b,10]
+            # label     [b]
+            loss = criteon(logits,label)
+            loss_sum.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        #完成了一个epoch
+        print(f"epoch:{epoch},loss:{np.mean(loss_sum)}")
+        print(f"epoch:{epoch},loss:{np.mean(loss_sum)}", file=f)
+        model.eval()#测试模式
+        with torch.no_grad():
+            total_correct = 0
+            total_num = 0
+            for x, label in cifar_test:
+                x, label = x.to(device), label.to(device)
+                #[b,10]
+                logits = model(x)
+                #[b]
+                pred = logits.argmax(dim=1)
+                #[b] vs [b]
+                total_correct += torch.eq(pred, label).float().sum().item()
+                total_num += x.size(0)
+            acc = total_correct / total_num
+            print(f"epoch:{epoch},acc:{acc}")
+            print(f"epoch:{epoch},acc:{acc}", file=f)
+            f.close()
+if __name__ == '__main__':
+    main()
